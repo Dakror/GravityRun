3047da8a0e0f6d22c9b8f30b335ba38828c37d31
Dakror <mail@dakror.de>
2014-12-08 18:10:03 +0100

    libgdx setup

---
+++ desktop/src/de/dakror/gravityrun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.dakror.gravityrun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.dakror.gravityrun.GravityRun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GravityRun(), config);
+	}
+}
